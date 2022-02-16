ec40a0a7baadf128e89cbde4ac2bff51dd08aff4
会远 <jiezhouwork@gmail.com>
2022-02-16 15:11:25 +0800

    feat: 引入路径更新

--- src/module/RegisterModule.ts
+++ src/module/RegisterModule.ts
@@ -2,7 +2,7 @@
  * @Description: 此模块用于注册一些第三方服务，所有的服务都放置于此注册。例：Redis、TypeOrm等
  * @Author: zhoujie
  * @Date: 2021-12-27 17:08:32
- * @LastEditTime: 2022-02-16 14:31:19
+ * @LastEditTime: 2022-02-16 15:11:08
  * @LastEditors: zhoujie
  */
 import { Global, Logger, Module, ValidationPipe } from '@nestjs/common';
@@ -11,7 +11,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
 import DatabaseConfig from 'src/config/DatabaseConfig';
 import { APP_GUARD, APP_PIPE } from '@nestjs/core';
-import { RoleGuard } from 'src/guard/RoleGuard';
+import { RoleGuard } from 'src/auth/guard/RoleGuard';
 
 @Module({
   imports: [
